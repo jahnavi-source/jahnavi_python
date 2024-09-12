6ce330f6ed2d25301c2278982176d8c1566d864f
jahnavi <jahnavij78@gmail.com>
2024-09-12 16:21:33 -0500

    commiting code with example

---
+++ code.py
@@ -0,0 +1,9 @@
+"""
+write a program (a+b)*2
+
+
+"""
+a=2
+b=3
+result = (a + b) * 2
+print("The result of (a + b) * 2 is:", result)
---
+++ example.py
@@ -0,0 +1,10 @@
+"""
+write a program simple interest
+si=p*t*r/100
+
+"""
+p=10000
+t=365
+r=5
+si=(p*t*r)/100
+print(si)
